2cde791e0e7501d13cc8bcdea750e15d4eb010b6
KalyanPaladugu <31949037+KalyanPaladugu@users.noreply.github.com>
2020-05-04 12:10:37 +0530

    updating

--- conf.py
+++ conf.py
@@ -24,18 +24,18 @@ author = 'Kalyan Paladugu'
 # The full version, including alpha/beta/rc tags
 release = '1'
 
-
+import sphinx_rtd_theme
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = [ 'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-source_suffix = '.rst'
+source_suffix = ['.md','.rst']
 master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
@@ -49,7 +49,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
